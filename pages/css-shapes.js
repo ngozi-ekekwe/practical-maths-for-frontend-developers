2a5cd48c852ac754b3c591234a78d307154122e1
Ngozi Ekekwe <ngozi-ekekwe>
2020-11-03 21:21:49 +0100

    add shapes

--- pages/css-shapes.js
+++ pages/css-shapes.js
@@ -1,7 +1,8 @@
 import Head from "next/head";
 import Header from "@components/Header";
 import Section from "@components/Section";
-import BreadCrumbs from '@components/Breadcrumbs';
+import BreadCrumbs from "@components/Breadcrumbs";
+import Hint from "@components/Hint";
 import Footer from "@components/Footer";
 
 export default function CSSShapes() {
@@ -15,7 +16,24 @@ export default function CSSShapes() {
       <Header></Header>
       <BreadCrumbs />
       <main>
-        <Section title="CSS SHAPES" />
+        <Section title="🔺 CSS Shapes 🔵" />
+        <Hint>
+          <p>Shapes you can make with CSS</p>
+        </Hint>
+        <div className="shape-container">
+          <div>
+            <div class="square"></div>
+          </div>
+          <div>
+            <div class="circle"></div>
+          </div>
+          <div>
+            <div class="diamond"></div>
+          </div>
+          <div>
+            <div class="triangle"></div>
+          </div>
+        </div>
       </main>
       <Footer></Footer>
     </>
