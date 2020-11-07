a3e861e36417e95bdc14eb6e7d3a7fa9492c2938
Ngozi Ekekwe <ngozi-ekekwe>
2020-11-07 12:08:58 +0100

    add monthly expense total

--- pages/monthly-expense-sheet.js
+++ pages/monthly-expense-sheet.js
@@ -15,6 +15,26 @@ export default function MonthlyExpenseSheet() {
     return nextDay;
   }
 
+  // Generate a Monthly Budget (Subtract Rent/Utils)
+  let annualIncome = roll(31000, 40000, 1).toFixed(2);
+  let monthlyIncome = parseFloat(annualIncome) / 12;
+  let rent = roll(1200, 1800, 1).toFixed(2);
+  let utilities = roll(300, 500).toFixed(2);
+
+  let monthlyBudget = monthlyIncome - parseFloat(rent) - parseFloat(utilities);
+  
+  function generateExpenses() {
+    let expenseCount = roll(0, 4);
+    return [...Array(expenseCount)].map((_, i) => {
+      return {
+        index: i,
+        value: roll(1, 30, 1),
+      };
+    });
+  }
+
+  
+
   // Generate an Array of all Days in the Month of a given date
   function buildMonth(day) {
     let daysInMonth = getDaysInMonth(day.getMonth() + 1, day.getFullYear());
@@ -24,25 +44,61 @@ export default function MonthlyExpenseSheet() {
       let monthDay = {
         index: i,
         day: iterableDate,
-        expense: []
-      }
+        expenses: generateExpenses(),
+      };
       iterableDate = getNextDay(iterableDate);
-      return monthDay
-    })
-    return month
+      return monthDay;
+    });
+    return month;
   }
 
   // Write a function to get the number of days in a given month
   function getDaysInMonth(month, year) {
-    return new Date(year, month, 0).getDate(); 
+    return new Date(year, month, 0).getDate();
   }
 
-  let month = buildMonth(startDay)
+  let month = buildMonth(startDay);
+
+  // Calculate / Display Leftover Cash
+  function getMonthNetValue() {
+    let monthlyExpenseTotal = month.reduce((acc, simpleDay) => {
+      return acc + getDailyCost(simpleDay)
+    }, 0)
+
+    return monthlyBudget - monthlyExpenseTotal;
+  }
+
+
+  function getDailyCost(day) {
+    return day.expenses.reduce((acc, expense) => {
+      return acc + parseFloat(expense.value)
+    }, 0)
+  }
+
+  let displayExpenses = (expenses) => {
+    return expenses.reduce((acc, expense) => {
+      return acc + `<div class="expense"> -\$${expense.value.toFixed(2)}</div>`;
+    }, "");
+  };
+
+  let monthlyNetValue = getMonthNetValue()
+
+  console.log(monthlyNetValue, 'monthlyNetValue')
 
   // Write a function to display the month
   let printMonth = month.reduce((acc, currentDay) => {
-    return acc + `<div class="day">${currentDay.day.getDate()}</div>`
-  }, '')
+    return (
+      acc +
+      `
+      <div class="day">
+      ${currentDay.day.getDate()}
+      ${displayExpenses(currentDay.expenses)}
+      </div>
+     `
+    );
+  }, "");
+
+
 
   return (
     <>
@@ -57,6 +113,9 @@ export default function MonthlyExpenseSheet() {
         <Section title="Monthly Expense Sheet 📅">
           <Hint>Monthly Expense Sheet</Hint>
           <div className="month-schedule">
+            <div className="monthly-summary">
+              Monthly Budget ${monthlyBudget.toFixed(2)} | Net Value ${monthlyNetValue.toFixed(2)}
+            </div>
             <div dangerouslySetInnerHTML={{ __html: printMonth }}></div>
           </div>
         </Section>
